209b75c6c29b1b8bea46b5c51635acfe65ba1ed8
xhrbug <190053919+xhrbug@users.noreply.github.com>
2024-12-09 14:24:12 +0700

    add buondua

---
+++ buondua.js
@@ -0,0 +1,76 @@
+function getPosts(url, page) {
+  console.log(url);
+
+  var doc = fetch(url);
+  var listElement = doc.select("div.blog div.items-row");
+  var posts = [];
+  listElement.forEach((element) => {
+    posts.push({
+      name: element
+        .select("div.item-content div.page-header h2 a.item-link")
+        .first()
+        .text(),
+      url: element
+        .select("div.item-content div.page-header h2 a.item-link")
+        .first()
+        .absUrl("href"),
+      thumbnail: element
+        .select("div.item-thumb a.item-link.popunder img")
+        .first()
+        .attr("src"),
+    });
+  });
+
+  const lastPage = doc.select("ul.pagination-list li a.pagination-link").last();
+  const total = lastPage ? parseInt(lastPage.text()) : 1;
+
+  var nextPage = doc.select("nav.pagination a.pagination-next").first();
+  nextPage = nextPage ? nextPage.absUrl("href") : null;
+
+  console.log(nextPage);
+
+  return {
+    posts: posts,
+    total: total,
+    next: nextPage,
+  };
+}
+
+function getImages(url, page) {
+  console.log(url);
+  var doc = fetch(url);
+
+  var urls = [];
+  var images = doc.select("div.article.content div.article-fulltext p img");
+  images.forEach((image) => {
+    const src = image.attr("src");
+    urls.push(src);
+  });
+
+  var total = doc
+    .select("nav.pagination div.pagination-list span a.pagination-link")
+    .last();
+
+  total = total ? parseInt(total.text()) : 1;
+
+  var next = doc
+    .select(
+      "nav.pagination div.pagination-list span a.pagination-link.is-current"
+    )
+    .last();
+  next = next ? next.parent().nextElementSibling() : null;
+
+  return {
+    images: urls,
+    total: total,
+    next: next,
+  };
+}
+
+function search(queryUrl, page) {
+  return getPosts(queryUrl, page);
+}
+
+function getSearchUrl(query) {
+  return baseUrl + "?search=" + query;
+}
